49747127b6060c3b2b7e4d30fd427eeff2fdb6e9
Samuelbeckley <46754895+Samuelbeckley@users.noreply.github.com>
2020-03-26 23:30:10 -0400

    Money script and changed makeplaceable to track clones

--- Assets/Scripts/MakePlaceable.cs
+++ Assets/Scripts/MakePlaceable.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MakePlaceable : MonoBehaviour
 {
-
     [SerializeField]
     private GameObject finalObject;
     private Vector3 mousePos;
@@ -15,43 +14,49 @@ public class MakePlaceable : MonoBehaviour
     [SerializeField]
     private LayerMask ICONLayer;
 
+    [SerializeField]
+    private string linkedCount;
 
-    public bool hasStarted;
-    public int numOfFacs = 10;
 
     void Start()
     {
-        if(hasStarted == false)
-        {
-          numOfFacs = 0;
-        }
-    }
 
+    }
+    void UpdateCount(){
+      GameObject MoneyListener = GameObject.Find("Money Listener");
+      Money moneyScript = MoneyListener.GetComponent<Money>();
+      if(linkedCount == "Factories") {
+        moneyScript.Factories++;
+      } else if (linkedCount == "Mines") {
+        moneyScript.Mines++;
+      } else if (linkedCount == "Fish") {
+        moneyScript.Fish++;
+      } else if (linkedCount == "Wood") {
+        moneyScript.Wood++;
+      }
+    }
+    // Update is called once per frame
     void Update()
     {
-
-
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);//get mouse pos
         transform.position = new Vector3(mousePos.x, mousePos.y, -1);//put object on mouse, -1 z for layering; could be vector 2 otherwise
 
         if(Input.GetMouseButtonDown(0)) //leftclick
         {
-          
+
+
           RaycastHit2D landHit = Physics2D.Raycast(mousePos,Vector2.zero, Mathf.Infinity,BGLayer);
           //is mouse on continent
           RaycastHit2D iconHit = Physics2D.Raycast(mousePos,Vector2.zero, Mathf.Infinity,ICONLayer);
-          
           //is mouse on icon
           if (iconHit.collider == null && landHit.collider != null) {
             Instantiate(finalObject, transform.position, Quaternion.identity);
             //create final object, at mouse position, maintain rotation
-            numOfFacs++;
-            hasStarted = true;
-            Debug.Log(numOfFacs);
+            UpdateCount();
+
           }
 
         } else if (Input.GetMouseButtonDown(1)) { //right click
-			  Cursor.visible = true;
           Destroy(this.gameObject); //remove spawner from mouse
         }
     }
---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public int Currency = 0, Fish = 0, Wood = 0, Factories = 0, Mines = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate(){
+    Currency += Factories;
+    Debug.Log(Currency);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
