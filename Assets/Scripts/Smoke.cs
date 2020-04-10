6196332f2670205b737977e9b2fa31e5a4843133
Samuelbeckley <46754895+Samuelbeckley@users.noreply.github.com>
2020-04-10 12:23:02 -0400

    fixed smoke sortingOrder

---
+++ Assets/Scripts/Smoke.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject Cam = GameObject.Find("ContinentListeners");
+        //SelectContinent continentScript = Cam.GetComponent<SelectContinent>();
+        //GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingLayerName = sortingLayerName;
+        Debug.Log(gameObject.name);
+        //VisualEffect ps = gameObject.GetComponent<VisualEffect>();
+        Renderer r = gameObject.GetComponent<Renderer>();
+        r.sortingLayerName = "Top";
+        r.sortingOrder = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
