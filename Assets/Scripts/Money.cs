1c78b86a73906bf975e92365c47916c2e19d500a
Samuelbeckley <46754895+Samuelbeckley@users.noreply.github.com>
2020-04-10 11:58:31 -0400

    boundary bugfix; money gaining delayed

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -30,11 +30,11 @@ public class Money : MonoBehaviour
     public float mineF;
     public float mineRound;
 
-    
+
 
     void Start()
     {
-      
+
       fishPrice.text = "" + 5;
       woodPrice.text = "" + 5;
       factoryPrice.text = "" + 20;
@@ -48,7 +48,7 @@ public class Money : MonoBehaviour
     void FixedUpdate()
     {
         CurrencyF += Factories / 50;
-        CurrencyF += Time.deltaTime * (1 + Factories + Wood * 4 / 3 + Fish * 4 / 3 + Mines * 4 / 3);
+        CurrencyF += Time.deltaTime * ( Factories + Wood * 4 / 3 + Fish * 4 / 3 + Mines * 4 / 3);
         CurrencyRound = Mathf.RoundToInt(CurrencyF);
         Currency.text = "" + CurrencyRound;
     }
